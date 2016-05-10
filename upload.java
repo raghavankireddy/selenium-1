cd27bfe2985ca9cb33f89d8316827c5a305e56db
schaubut <schaubut@me.com>
2016-05-10 17:07:56 -0400

    New Files
    Drag and Drop not completed because selenium draganddrop broken HTML5
    firefox and chrome.
    Upload completed

---
+++ upload.java
@@ -0,0 +1,52 @@
+package selenium.selenium;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class upload {
+	public static WebDriver driver;	
+	Utilities utility = new Utilities();
+	static Initialize init = new Initialize();	
+	static String browserDriverType = "FireFoxDriver";
+	WebElement item;
+	
+    @BeforeClass
+	public static void start() throws Exception {
+		System.out.println("The start");    	
+    	Initialize init = new Initialize();
+		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    	driver = init.setUp( browserDriverType,"http://the-internet.herokuapp.com/upload" );
+    }
+ 
+	@AfterClass
+	public static void end(){
+		System.out.println("The end");
+		init.teardown(driver);
+	}
+	
+	@Test
+	public void testA(){
+		System.out.println("File Uploader page test");	
+		WebElement item;
+		item = driver.findElement(By.xpath(".//*[@id='file-upload']"));
+		item.sendKeys("c:\\commands\\testupload.txt");
+		item = driver.findElement(By.xpath(".//*[@id='file-submit']"));
+		item.click();
+		item = driver.findElement(By.id("uploaded-files"));		
+		assertTrue(item.getText().contains("testupload.txt"));
+	}
+	
+	
+	
+	
+}
